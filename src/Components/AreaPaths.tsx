aae242bde340e2ddafd353adb9f2337d467156a6
Yucatta <ahmetabali41@gmail.com>
2025-07-26 16:10:57 +0300

    .

--- src/Components/AreaPaths.tsx
+++ src/Components/AreaPaths.tsx
@@ -1,22 +1,30 @@
-import { useDataContext } from "@/context/DataContext";
-import React, { useMemo } from "react";
-const AreaOutlines = () => {
-  const { areapaths } = useDataContext();
-  const areas = useMemo(() => {
-    return areapaths.map((patharray) =>
-      patharray[1].map((path, index) => (
-        <path
-          d={path}
-          fill={"none"}
-          stroke="rgb(20,20,20)"
-          strokeWidth="0.5"
-          className="pointer-events-none"
-          key={index}
-        ></path>
-      ))
-    );
-  }, [areapaths]);
-  return <>{areas}</>;
+// import { useDataContext } from "@/context/DataContext";
+// import React, { useMemo } from "react";
+// const AreaOutlines = () => {
+//   const { areapaths } = useDataContext();
+//   const areas = useMemo(() => {
+//     return areapaths.map((patharray) =>
+//       patharray[1].map((path, index) => (
+//         <path
+//           d={path}
+//           fill={"none"}
+//           stroke="rgb(20,20,20)"
+//           strokeWidth="0.5"
+//           className="pointer-events-none"
+//           key={index}
+//         ></path>
+//       ))
+//     );
+//   }, [areapaths]);
+//   return <>{areas}</>;
+// };
+
+// export default AreaOutlines;
+
+import React from "react";
+
+const AreaPaths = () => {
+  return <div>AreaPaths</div>;
 };
 
-export default AreaOutlines;
+export default AreaPaths;
