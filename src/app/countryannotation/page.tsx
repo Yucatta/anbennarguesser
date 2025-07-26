c003d3ba4b295243c1cdfde73853c71bd4e1a3f3
Yucatta <ahmetabali41@gmail.com>
2025-07-26 17:39:10 +0300

    it wasnt it

--- src/app/countryannotation/page.tsx
+++ src/app/countryannotation/page.tsx
@@ -1,16 +1,23 @@
-import { GameContextProvider } from "@/context/GameContext";
-import { MapContextProvider } from "@/context/MapContext";
+// import { GameContextProvider } from "@/context/GameContext";
+// import { MapContextProvider } from "@/context/MapContext";
+// import React from "react";
+// import AnnotationWrapper from "./AnnotationWrapper";
+
+// const page = () => {
+//   return (
+//     <GameContextProvider>
+//       <MapContextProvider>
+//         <AnnotationWrapper></AnnotationWrapper>
+//       </MapContextProvider>
+//     </GameContextProvider>
+//   );
+// };
+
+// export default page;
 import React from "react";
-import AnnotationWrapper from "./AnnotationWrapper";
 
 const page = () => {
-  return (
-    <GameContextProvider>
-      <MapContextProvider>
-        <AnnotationWrapper></AnnotationWrapper>
-      </MapContextProvider>
-    </GameContextProvider>
-  );
+  return <div>page</div>;
 };
 
 export default page;
